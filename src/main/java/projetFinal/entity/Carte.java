d403e8b292cbe8a7a9d672a48ff55a687f8cad52
onitokill <nina.luz@hotmail.fr>
2021-11-16 15:33:18 +0100

    getter setter carte

--- src/main/java/projetFinal/entity/Carte.java
+++ src/main/java/projetFinal/entity/Carte.java
@@ -1,5 +1,6 @@
 package projetFinal.entity;
 
+import java.util.Objects;
 import java.util.Set;
 
 import javax.persistence.Column;
@@ -12,6 +13,34 @@ public class Carte {
 	private Restaurant restaurant;
 	@Column(name = "carte_plats")
 	private Set<Plat> plats;
+	
+	public Restaurant getRestaurant() {
+		return restaurant;
+	}
+	public void setRestaurant(Restaurant restaurant) {
+		this.restaurant = restaurant;
+	}
+	public Set<Plat> getPlats() {
+		return plats;
+	}
+	public void setPlats(Set<Plat> plats) {
+		this.plats = plats;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(plats, restaurant);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Carte other = (Carte) obj;
+		return Objects.equals(plats, other.plats) && Objects.equals(restaurant, other.restaurant);
+	}
 	
 
 }
