8867208d2efe8e78f251ee0beb8211f577aad8e0
Quentin <quentin.richard@ecam-rennes.com>
2021-11-22 14:57:50 +0100

    classes test

---
+++ src/test/java/testService/CommandeServiceTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package testService;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author quent
+ *
+ */
+public class CommandeServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
