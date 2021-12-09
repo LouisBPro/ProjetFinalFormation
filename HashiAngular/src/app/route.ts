4249ed093b9f3393ea2d02df5e913324283bb8bf
onitokill <nina.luz@hotmail.fr>
2021-12-09 13:56:59 +0100

    Revert "fix home"
    This reverts commit 50ae0ede444759eb1396cf97c9f5db71ec3eac13.

--- HashiAngular/src/app/route.ts
+++ HashiAngular/src/app/route.ts
@@ -1,3 +1,4 @@
-import { HomeComponent } from "./home/home.component";
-import { Routes } from "@angular/router";
-export const route: Routes = [{ path: "home", component: HomeComponent }];
+import { Routes } from '@angular/router';
+export const route : Routes =[
+
+];
