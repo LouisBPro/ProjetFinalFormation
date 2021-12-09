6f8674c552597eff5d47ce24fa6707326e8b804a
Quentin <quentin.richard@ecam-rennes.com>
2021-12-09 17:38:11 +0100

    client component fonctionnel

--- HashiAngular/src/app/client/client/client.component.ts
+++ HashiAngular/src/app/client/client/client.component.ts
@@ -1,15 +1,32 @@
+import { ActivatedRoute } from '@angular/router';
+import { Client } from './../../model/client';
+import { ClientService } from './../../services/client.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
-  styleUrls: ['./client.component.css']
+  styleUrls: ['./client.component.css'],
 })
 export class ClientComponent implements OnInit {
+  client: Client = new Client();
 
-  constructor() { }
+  constructor(
+    private clientService: ClientService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.initClient();
   }
 
+  initClient() {
+    this.activatedRoute.params.subscribe((params) => {
+      if (!!params['client']) {
+        this.clientService.byId(params['client']).subscribe((client) => {
+          this.client = client;
+        });
+      }
+    });
+  }
 }
