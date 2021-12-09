50ae0ede444759eb1396cf97c9f5db71ec3eac13
onitokill <nina.luz@hotmail.fr>
2021-12-09 13:56:59 +0100

    fix home

--- HashiAngular/src/app/route.ts
+++ HashiAngular/src/app/route.ts
@@ -1,4 +1,3 @@
-import { Routes } from '@angular/router';
-export const route : Routes =[
-
-];
+import { HomeComponent } from "./home/home.component";
+import { Routes } from "@angular/router";
+export const route: Routes = [{ path: "home", component: HomeComponent }];
