e870a8525bbbf8df30fabe62e6c179dc46e34c3e
Quentin <quentin.richard@ecam-rennes.com>
2021-12-13 18:07:57 +0100

    ajout tous les files pour ligneCommande

---
+++ HashiAngular/src/app/model/ligne-commande-pk.spec.ts
@@ -0,0 +1,7 @@
+import { LigneCommandePk } from './ligne-commande-pk';
+
+describe('LigneCommandePk', () => {
+  it('should create an instance', () => {
+    expect(new LigneCommandePk()).toBeTruthy();
+  });
+});
